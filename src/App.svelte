4f145b0472e9412fe994f755aa6c0dada7977e71
babakfp <babak.bxf@gmail.com>
2021-11-29 19:10:52 +0330

    Formatted import order

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
 	import Simple from './lib/Simple.svelte'
 	import NoWrapper from './lib/NoWrapper.svelte'
-	import UseWrapper from './lib/UseWrapper.svelte'
 	import ExcludedElement from './lib/ExcludedElement.svelte'
 	import IncludeSelf from './lib/IncludeSelf.svelte'
+	import UseWrapper from './lib/UseWrapper.svelte'
 	import WrapperClass from './lib/WrapperClass.svelte'
 </script>
 
