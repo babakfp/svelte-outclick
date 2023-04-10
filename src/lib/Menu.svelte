a3cc09ef44ddf95241550dd88036814fe4788c94
babakfp <44144724+babakfp@users.noreply.github.com>
2023-04-10 13:58:34 -0700

    Formatted

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -20,9 +20,7 @@
 	on:click={() => (isOpen = !isOpen)}
 >
 	<!-- prettier-ignore -->
-	<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-		<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
-	</svg>
+	<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5"/></svg>
 </button>
 
 <OutClick on:outclick={() => (isOpen = false)} excludeElements={toggleBtn}>
