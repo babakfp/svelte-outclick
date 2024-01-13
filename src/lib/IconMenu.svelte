ae5f9a6b2659fc41cbef57e85a1a712ee44dceb6
babakfp <babak.bxf@gmail.com>
2024-01-13 12:42:46 -0800

    Changed menu icon

--- src/lib/IconMenu.svelte
+++ src/lib/IconMenu.svelte
@@ -4,4 +4,4 @@
 </script>
 
 <!-- prettier-ignore -->
-<svg class="icon {className}" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5"/></svg>
+<svg class="icon {className}" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#7ea602" viewBox="0 0 256 256"><path d="M216,64V192H40V64Z" opacity="0.2"></path><path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path></svg>
